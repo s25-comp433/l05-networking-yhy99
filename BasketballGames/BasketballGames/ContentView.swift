8bd460b770c5f91d11810292357b15baf7f3e487
yhy99 <yhy@unc.edu>
2025-03-01 23:41:43 -0500

    1

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,50 +7,44 @@
 
 import SwiftUI
 
-// 定义 JSON 数据结构
-struct Game: Codable, Identifiable {
+struct Game: Codable {
     var id: Int
-    var team: String // "Men" or "Women"
+    var team: String
     var opponent: String
     var date: String
     var isHomeGame: Bool
     var score: Score
+}
 
-    struct Score: Codable {
-        var unc: Int
-        var opponent: Int
-    }
+struct Score: Codable {
+    var unc: Int
+    var opponent: Int
 }
 
 struct ContentView: View {
     @State private var games = [Game]()
-    
+
     var body: some View {
-        NavigationView {
-            List(games) { game in
+        NavigationStack {
+            List(games, id: \.id) { game in
                 HStack {
-                    VStack(alignment: .leading, spacing: 5) {
+                    VStack(alignment: .leading) {
                         Text("\(game.team) vs. \(game.opponent)")
                             .font(.headline)
-                        
                         Text(game.date)
                             .font(.subheadline)
                             .foregroundColor(.gray)
                     }
-                    
                     Spacer()
-                    
-                    VStack(alignment: .trailing, spacing: 5) {
+                    VStack(alignment: .trailing) {
                         Text("\(game.score.unc) - \(game.score.opponent)")
                             .font(.headline)
                             .bold()
-                        
                         Text(game.isHomeGame ? "Home" : "Away")
                             .font(.subheadline)
                             .foregroundColor(.gray)
                     }
                 }
-                .padding(10)
             }
             .navigationTitle("UNC Basketball")
         }
@@ -58,22 +52,19 @@ struct ContentView: View {
             await loadData()
         }
     }
-    
+
     func loadData() async {
         guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
             print("Invalid URL")
             return
         }
-        
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-            let decodedResponse = try JSONDecoder().decode([Game].self, from: data)
-            
-            DispatchQueue.main.async {
-                self.games = decodedResponse
+            if let decodedResponse = try? JSONDecoder().decode([Game].self, from: data) {
+                games = decodedResponse
             }
         } catch {
-            print("Error fetching data: \(error.localizedDescription)")
+            print("Invalid data")
         }
     }
 }
