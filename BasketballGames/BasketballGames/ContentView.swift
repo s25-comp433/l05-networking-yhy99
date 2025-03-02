d197bffc53d99440be8dc2c02cf9427bd2705ef4
yhy99 <yhy@unc.edu>
2025-03-01 23:10:08 -0500

    1

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,74 @@
 
 import SwiftUI
 
+// 定义 JSON 数据结构
+struct Game: Codable, Identifiable {
+    var id: Int
+    var team: String // "Men" or "Women"
+    var opponent: String
+    var date: String
+    var isHomeGame: Bool
+    var score: Score
+
+    struct Score: Codable {
+        var unc: Int
+        var opponent: Int
+    }
+}
+
 struct ContentView: View {
+    @State private var games = [Game]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(games) { game in
+                HStack {
+                    VStack(alignment: .leading, spacing: 5) {
+                        Text("\(game.team) vs. \(game.opponent)")
+                            .font(.headline)
+                        
+                        Text(game.date)
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .trailing, spacing: 5) {
+                        Text("\(game.score.unc) - \(game.score.opponent)")
+                            .font(.headline)
+                            .bold()
+                        
+                        Text(game.isHomeGame ? "Home" : "Away")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(10)
+            }
+            .navigationTitle("UNC Basketball")
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let decodedResponse = try JSONDecoder().decode([Game].self, from: data)
+            
+            DispatchQueue.main.async {
+                self.games = decodedResponse
+            }
+        } catch {
+            print("Error fetching data: \(error.localizedDescription)")
+        }
     }
 }
 
--- iTunesSearch/iTunesSearch/ContentView.swift
+++ iTunesSearch/iTunesSearch/ContentView.swift
@@ -1,21 +1,51 @@
 //
 //  ContentView.swift
-//  iTunesSearch
+//  itune
 //
-//  Created by Samuel Shi on 2/27/25.
+//  Created by xiuxiu的ipad on 3/1/25.
 //
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 struct ContentView: View {
+    @State private var results = [Result]()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(results, id: \.trackId) {
+            item in VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
         }
-        .padding()
+        .task {
+            await loadData()
+        }
+    }
+
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
